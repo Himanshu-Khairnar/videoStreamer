f2e10ad82ebe68d57a411f588b33770d40beb2d7
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-10-04 15:00:48 +0530

    Utils changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,25 +1,15 @@
 import dotenv from 'dotenv'
 import ConnectDB from './db/index.js'
+
 dotenv.config({
-path:'./env'
+    path: './env'
 })
 
-ConnectDB()
 
-// import express from 'express'
-// const app=express()
-// ;( async()=>{
-//     try {
-//       await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//       app.on('error',(error)=>{
-//         console.log(error)
-//         throw error
-//       })
-//       app.listen(process.env.PORT,()=>{
-//           console.log("App is listening on port "+process.env.PORT) 
-//       })
-//     } catch (error) {
-//         console.log(error)
-//         throw error
-//     }
-// })()
+
+ConnectDB().then(() => {
+    app.listen((process.env.PORT || 8000), () => {
+        console.log("Server is running on port " + process.env.PORT || 8000)
+    })
+    app.on("error", (error) => console.log(error))
+}).catch((error) => console.log(error))
