aed35beea08ff514c2fb4a3276343c26b49684c4
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-10-10 14:31:44 +0530

    Added getWatchHistory

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js'
 import { OldImagetoDelete, uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import jwt from "jsonwebtoken"
+import mongoose from 'mongoose'
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -313,34 +314,79 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
                 subscribersCount: { $size: "$subscribers" },
                 subscriptionsCount: { $size: "$subscriptions" },
                 isSubscribed: {
-                    $cond:{
-                        if:{$in:[req.user?._id, "$subscribers"]},
-                        then:true,
-                        else:false
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers"] },
+                        then: true,
+                        else: false
                     }
                 }
             }
         },
         {
             $project: {
-                fullName:1,
-                userName:1,
-                subscribersCount:1,
-                subscriptionsCount:1,
-                isSubscribed:1,
-                avatar:1,
-                coverImage:1,
-                email:1
+                fullName: 1,
+                userName: 1,
+                subscribersCount: 1,
+                subscriptionsCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
             }
         }
-        
+
     ])
-    if(!channel?.length)
+    if (!channel?.length)
         throw new ApiError(404, "Channel not found")
-    
+
     return res.
-    status(200).
-    json(new ApiResponse(200,channel[0],"User Channel"))
+        status(200).
+        json(new ApiResponse(200, channel[0], "User Channel"))
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changecurrentPassword, getCurrentUser, updateAccountDetails, updateUserCoverImage, updateUserAvatar, getUserChannelProfile }
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        userName: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                    , {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res.return.status(200).json(new ApiResponse(200, user[0].WatchHistory, "Watch History"))
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changecurrentPassword, getCurrentUser, updateAccountDetails, updateUserCoverImage, updateUserAvatar, getUserChannelProfile, getWatchHistory }
