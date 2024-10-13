71fd89f89df217ca3d54581c189908176313cd3e
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-10-13 17:07:35 +0530

    Completed like controller endpoints

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -5,25 +5,81 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: toggle like on video
+    try {
+        const { videoId } = req.params
+        const { userId } = req.body
+        let like
+        let toggleVideoLike
+
+        if (!isValidObjectId(videoId) || !isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid videoId or userId");
+        }
+
+        const videos = await Like.findOne({ video: videoId, likedBy: userId })
+        if (!videos) {
+            toggleVideoLike = await Like.create({
+                video: videoId,
+                likedBy: userId
+            })
+            like = true
+        }
+        else {
+            toggleVideoLike = await Like.findOneAndDelete({ video: videoId, likedBy: userId })
+            like = false
+        }
+
+        return res.status(200).json(new ApiResponse(200, toggleVideoLike, `${like ? "Video liked" : "Video unliked"}`))
+    } catch (error) {
+        throw new ApiError(500, "Error while toggling video like")
+    }
+
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const { commentId } = req.params
-    //TODO: toggle like on comment
+    try {
+        const { commentId } = req.params
+        const { userId } = req.body
+        let toggleCommentLike
+        let like
 
+        if (!isValidObjectId(commentId) || !isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid videoId or userId");
+        }
+
+        const comment = await Comment.findOne({ comment: commentId, likedBy: userId })
+        if (!comment) {
+            toggleCommentLike = await Like.create({
+                comment: commentId,
+                likedBy: userId
+            })
+            like = true
+        }
+        else {
+            toggleCommentLike = await Like.findOneAndDelete({ comment: commentId, likedBy: userId })
+            like = false
+
+        }
+
+        return res.status(200).json(new ApiResponse(200, toggleCommentLike, `${like ? "Comment liked" : "Comment unliked"}`))
+
+    } catch (error) {
+        throw new ApiError(500, "Error while toggling comment like")
+    }
 })
 
-
-
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    try {
+        const { userId } = req.params
+        const videos = await Like.find({ likedBy: userId })
+        return res.status(200).json(new ApiResponse(200, videos, "Videos found"))
+    } catch (error) {
+        throw new ApiError(500, "Error while getting liked videos")
+    }
 })
 
 export {
     toggleCommentLike,
-
     toggleVideoLike,
     getLikedVideos
 }
