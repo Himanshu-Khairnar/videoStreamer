ccc26a45deb66f233a7119f0ec2c18069de86991
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-10-30 16:41:54 +0530

    Change frontend from react to next js and done with navbar and its responsive

---
+++ Frontend/src/Components/ui/Navbar.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import React, { useState } from 'react';
+import { Button } from "./button";
+import Image from 'next/image';
+import { Input } from './input';
+import { Search } from 'lucide-react';
+import { ListBulletIcon } from '@radix-ui/react-icons';
+
+const Navbar = () => {
+    const [toggle,settoggle]=useState(false)
+    return (
+        <div className='flex items-center justify-between p-4  '>
+            {/* Logo */}
+            <Image
+                src='/logo.svg'
+                height={50}
+                width={50}
+                alt='logo'
+                className='mr-4'
+            />
+            {/* Search Input and Button */}
+            <div className='flex items-center        w-[60rem] gap-3'>
+                <Input type='search' placeholder='Search...' className='flex-grow' />
+                <Button variant='ghost' className='hidden md:block'>
+                    <Search />
+                </Button>
+            </div>
+            {/* Buttons Container */}
+            <div className='flex items-center gap-4'>
+                {/* Login Button */}
+                <Button variant="ghost" className='hidden md:block text-base'>
+                    Log in &rarr;
+                </Button>
+                {/* List Bullet Icon Button */}
+                <Button variant='ghost' className='md:hidden' onClick={() => settoggle( !toggle)}>
+                    <ListBulletIcon className='h-6 w-6' />
+                </Button>
+                {toggle && (
+                    <ul className='absolute top-[5rem] right-[2rem] bg-gray-800 text-white border border-gray-700 rounded-md shadow-lg z-50'>
+                        <li className='px-4 py-2 hover:bg-gray-700 rounded-md cursor-pointer'>Liked Videos</li>
+                        <li className='px-4 py-2 hover:bg-gray-700 rounded-md cursor-pointer'>My Content</li>
+                        <li className='px-4 py-2 hover:bg-gray-700 rounded-md cursor-pointer'>Settings</li>
+                    </ul>
+                )}
+
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/Components/ui/Navbar'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='p-[1rem]'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
